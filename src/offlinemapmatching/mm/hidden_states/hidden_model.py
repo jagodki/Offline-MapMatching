fbc0bd15882a50628e8b816aca4e5999f1ca7e2d
Christoph Jung <jagodki.cj@gmail.com>
2018-10-05 21:22:20 +0200

    start work on curvature probability

--- src/offlinemapmatching/mm/hidden_states/hidden_model.py
+++ src/offlinemapmatching/mm/hidden_states/hidden_model.py
@@ -36,21 +36,22 @@ class HiddenModel:
             if len(candidates) == 0:
                 return -5
             else:
-                #create the current level of the trellis
-                current_trellis_level = []
+                #create the current level of the graph
+                current_graph_level = []
                 for candidate in candidates:
                     candidate.calculateEmissionProbability(observation, sigma, my)
-                    current_trellis_level.append({'id' : str(self.counter_candidates),
+                    current_graph_level.append({'id' : str(self.counter_candidates),
                                                   'observation_id' : observation.id,
                                                   'emitted_probability' : candidate.emission_probability,
                                                   'transition_probabilities' : {},
                                                   'transition_probability' : 0.0,
+                                                  'curvature_probability' : 0.0,
                                                   'total_probability' : 0.0})
                     self.candidates.update({str(self.counter_candidates) : candidate})
                     self.counter_candidates += 1
                 
-                #normalise the probabilities and add the current trellis level to the trellis
-                self.candidate_graph.append(current_trellis_level)
+                #normalise the probabilities and add the current graph level to the graph
+                self.candidate_graph.append(current_graph_level)
             
             #update progressbar
             self.updateProgressbar()
@@ -68,19 +69,19 @@ class HiddenModel:
         
         self.candidates_backtracking = {}
         
-        for i, trellis_level in enumerate(self.candidate_graph):
+        for i, graph_level in enumerate(self.candidate_graph):
             
             #the candidates of the first observation have no parent
             if i != 0:
-                for entry in trellis_level:
+                for entry in graph_level:
                     
                     #get all transition probabilities of the current entry and iterate over them to find the highest total probability
                     transition_probabilities = entry.get('transition_probabilities')
-                    for key in transition_probabilities:
-                        current_total_probability = transition_probabilities.get(key) * entry.get('emitted_probability') * self.getCandidateById(key, i - 1).get('total_probability')
+                    for key, value in transition_probabilities:
+                        current_total_probability = value * entry.get('emitted_probability') * self.getCandidateById(key, i - 1).get('total_probability')
                         if current_total_probability > entry.get('total_probability'):
                             entry.update({'total_probability' : current_total_probability})
-                            entry.update({'transition_probability' : transition_probabilities.get(key)})
+                            entry.update({'transition_probability' : value})
                             self.candidates_backtracking.update({entry.get('id') : key})
             
             #update progressbar
@@ -88,22 +89,28 @@ class HiddenModel:
         
         return 0
     
+    def setCurvatureProbability(self):
+        #iterate over all candidates
+        for i, current_candidate in self.candidate_graph:
+            if 0 < i < len(self.candidate_graph):
+    
+    
     def findViterbiPath(self):
         #init an array to store all candidates of the most likely path
         viterbi_path = []
         
-        #find the highest total probability in the last trellis level
+        #find the highest total probability in the last graph level
         highest_prob = 0.0
         id = None
-        trellis_counter = len(self.candidate_graph) - 1
-        last_trellis_level = self.candidate_graph[trellis_counter]
-        for entry in last_trellis_level:
+        graph_counter = len(self.candidate_graph) - 1
+        last_graph_level = self.candidate_graph[graph_counter]
+        for entry in last_graph_level:
             if entry.get('total_probability') >= highest_prob:
                 highest_prob = entry.get('total_probability')
                 id = entry.get('id')
         
         #add the last vertex of the path
-        last_candidate = self.getCandidateById(id, trellis_counter)
+        last_candidate = self.getCandidateById(id, graph_counter)
         viterbi_path.insert(0, {'vertex': self.candidates.get(id),
                                 'total_probability': highest_prob,
                                 'emitted_probability': last_candidate.get('emitted_probability'),
@@ -111,17 +118,17 @@ class HiddenModel:
                                 'observation_id': last_candidate.get('observation_id')})
         
         #now find all parents of this vertex/candidate
-        trellis_counter -= 1
+        graph_counter -= 1
         current_id = self.candidates_backtracking.get(id)
-        while(current_id is not None and trellis_counter >= 0):
-            searched_candidate = self.getCandidateById(current_id, trellis_counter)
+        while(current_id is not None and graph_counter >= 0):
+            searched_candidate = self.getCandidateById(current_id, graph_counter)
             viterbi_path.insert(0, {'vertex': self.candidates.get(current_id),
                                     'total_probability': searched_candidate.get('total_probability'),
                                     'emitted_probability': searched_candidate.get('emitted_probability'),
                                     'transition_probability': searched_candidate.get('transition_probability'),
                                     'observation_id': searched_candidate.get('observation_id')})
             current_id = self.candidates_backtracking.get(current_id)
-            trellis_counter -= 1
+            graph_counter -= 1
         
         return viterbi_path
     
@@ -134,17 +141,13 @@ class HiddenModel:
             #skip the first observation, because first observation has no parent
             if i != 0:
                 
-                #get the current and previous trellis level
-                previous_trellis_level = self.candidate_graph[i - 1]
-                current_trellis_level = self.candidate_graph[i]
+                #get the current and previous graph level
+                previous_graph_level = self.candidate_graph[i - 1]
+                current_graph_level = self.candidate_graph[i]
                 
-                for previous_entry in previous_trellis_level:
+                for previous_entry in previous_graph_level:
                     
-                    #init a variable to store the sum of transition probabilities starting from the previous_entry
-                    #to create a right stochastic matrix (that's not right, normalisation is commended!)
-                    sum_prob = 0.0
-                    
-                    for current_entry in current_trellis_level:
+                    for current_entry in current_graph_level:
                         
                         #get the candidates
                         current_candidate = self.candidates.get(current_entry.get('id'))
@@ -159,8 +162,7 @@ class HiddenModel:
                             transition.setRoutingProbability(self.network, observation.point.distance(self.trajectory.observations[i - 1].point))
                             transition.setTransitionProbability()
                             
-                            #insert the probability into the trellis and sum up them
-                            sum_prob += transition.transition_probability
+                            #insert the probability into the graph
                             current_entry.get('transition_probabilities').update({previous_entry.get('id') : transition.transition_probability})
                 
             self.updateProgressbar()
